fd77c5b04ef2b2fa60a2dd7d4b1b64d5ef0ed1e7
vedantlavale <vedantlavale@gmail.com>
2026-03-29 16:45:26 +0530

    update readme

--- test-db.js
+++ test-db.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
 async function test() {
-  await mongoose.connect("mongodb+srv://vedantlavale_db_user:sT58S2xn8Ryla5ig@cluster0.2xche4b.mongodb.net/", {
-    dbName: "reimbursement-management",
+  const uri = process.env.MONGODB_URI;
+  const dbName = process.env.MONGODB_DB_NAME;
+
+  if (!uri) {
+    throw new Error("Missing MONGODB_URI environment variable.");
+  }
+
+  await mongoose.connect(uri, {
+    dbName: dbName || undefined,
   });
+
   const db = mongoose.connection.db;
-  const users = await db.collection('users').find({}).toArray();
-  console.log(users.map((u) => ({ email: u.email, role: u.role, passwordHash: u.passwordHash })));
+  const users = await db.collection("users").find({}).toArray();
+  console.log(users.map((u) => ({ email: u.email, role: u.role })));
+
+  await mongoose.connection.close();
   process.exit(0);
 }
 test();
